c31026c7ecaef2bfcc656d7c2ff35f23eaeea67b
古明地こいし <zimamaxiaoan@outlook.com>
2025-04-21 21:09:08 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const timestamp = new Date().getTime();
     let bgUrl;
     if (window.innerWidth < 768) {
-        bgUrl = `https://api.maho.cc/random-img/mobile.php?t=${timestamp}`;
+        bgUrl = `[invalid url, do not cite]
     } else {
-        bgUrl = `https://moe.jitsu.top/img/?sort=pc&size=original&t=${timestamp}`;
+        bgUrl = `[invalid url, do not cite]&t=${timestamp}`;
     }
     document.body.style.backgroundImage = `url('${bgUrl}')`;
 });
--- main.js
+++ main.js
@@ -5,4 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }).then(container => {
         window.particlesContainer = container;
     });
+
+    // 头像旋转功能
+    const avatar = document.querySelector('.avatar');
+    avatar.addEventListener('mouseover', () => {
+        const direction = Math.random() < 0.5 ? 1 : -1;
+        avatar.style.transform = `rotate(${360 * direction}deg)`;
+    });
+    avatar.addEventListener('mouseout', () => {
+        avatar.style.transform = `rotate(0deg)`;
+    });
 });
